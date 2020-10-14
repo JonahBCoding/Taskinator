e5e1e359122856cb522f883778bc9d203354fb5c
unknown <jonah.benson@gmail.com>
2020-10-13 17:30:36 -0700

    adadadad

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,10 @@ var taskFormHandler = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
   var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
@@ -22,7 +26,7 @@ var createTaskEl = function (taskDataObj) {
   //create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type+ "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
 
   tasksToDoEl.appendChild(listItemEl);
